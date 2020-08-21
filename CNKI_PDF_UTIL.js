0e5080492fce2b0e0c737d02fd4a87350fc7c335
supernovaZhangJiaXing <supernovazhangjiaxing@gmail.com>
2020-08-21 14:09:50 +0800

    Update CNKI_PDF_UTIL.js

--- CNKI_PDF_UTIL.js
+++ CNKI_PDF_UTIL.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @id             CNKI_PDF_Supernova
 // @name           知网PDF下载助手
-// @version        1.1.0
+// @version        2.1.0
 // @namespace      https://github.com/supernovaZhangJiaXing/Tampermonkey/
 // @author         Supernova
 // @description    直接以PDF格式下载知网上的文献, 包括期刊论文和博硕士论文
-// @include        *://*.cnki.net/*
-// @include        *://*.cnki.net.*/*
+// @include        http*://*.cnki.net/*
+// @include        http*://*.cnki.net.*/*
 // @include        */kns/brief/*
 // @include        */kns55/brief/*
 // @include        */grid2008/brief/*
@@ -61,11 +61,8 @@ if (isDetailPage === false) {
 }
 // 如果进了详情页, 博硕士论文下面会出现四个按钮, 期刊论文下会有三个按钮
 else {
-    // 杂牌网站就是这个待遇
-    if (isCNKIPage === false) {
-        window.alert("建议移步CNKI主站下载");
-    }
-    else {
+    // 只对"博硕论文"详情页做优化, 否则影响期刊页面的显示
+    if (isCNKIPage === true && document.getElementById("catalog_Ptitle").innerHTML == "博硕论文") {
         ////////////////////////// downloadpart是正文的部分
         var downloadpart = document.getElementById("DownLoadParts");
         // 整本下载的按钮
@@ -83,15 +80,40 @@ else {
         dlpdf.href = pdf_link;
         dlpdf.classList.add('icon-dlpdf');
         dlpdf.classList.remove('icon-dlBlue');
-        // 分章下载我觉得没什么用所以我就替你们删掉了_(:з」∠)_
-        downloadpart.removeChild(downloadpart.childNodes[2]);
+        // 分章下载可以用来获取目录
+        var dlChapter = downloadpart.childNodes[2];
+        dlChapter.innerHTML = dlChapter.innerHTML.replace("分章", "目录");
+        dlChapter.classList.add('icon-dlcrsp');
+        dlChapter.classList.add('xml');
+        dlChapter.classList.remove('icon-dlBlue');
+        // 本来想做个ajax直接下载目录的, 发现不会写, 只能先跳转了
+//         // 目录页面的链接
+//         var cnt_link = dlChapter.href;
+//         // 获取页面目录信息并写入文本文件
+//         dlChapter.onclick=function(){
+//             // 没做IE6, IE5检查, 因为用脚本的估计没有上古IE用户
+//             var xmlhttp = new XMLHttpRequest();
+//             xmlhttp.onreadystatechange=function() {
+//                 if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+//                     console.log("我不知道该说什么, 先给大家拜个早年吧");
+//                 }
+//             }
+//             // 页面跳转了
+//             // href指向: /kns/download.aspx?filename=INDathWM4xke1k0QHRWTyN2UzFUctNmNQdkb2knRoNlTPd0aEpVcrt2VyN1KVFzU29GSSJEbRVmdZhFZkhnbxkUOp1ERiZEZhp0dTdDS5skUCRVMwEjZVVzLEF0LvZ3KWt2Ri9SWTBXY2plQadTdzxkVHFzMrtyQ&dflag=catalog&tablename=CDFDLAST2019&uid=WEEvREcwSlJHSldRa1FhcTdWa2FjR2F5a0d0dXBKMUw5SkR0NG9pWUFNMD0=$9A4hF_YAuvQ5obgVAqNKPCYcEjKensW4IQMovwHtwkF4VYPoHbKxJw!!
+//             // 新页面: https://kdoc.cnki.net/kdoc/download.aspx?filename=INDathWM4xke1k0QHRWTyN2UzFUctNmNQdkb2knRoNlTPd0aEpVcrt2VyN1KVFzU29GSSJEbRVmdZhFZkhnbxkUOp1ERiZEZhp0dTdDS5skUCRVMwEjZVVzLEF0LvZ3KWt2Ri9SWTBXY2plQadTdzxkVHFzMrtyQ&dflag=catalog&tablename=CDFDLAST2019&uid=WEEvREcwSlJHSldRa1FhcTdWa2FjR2F5a0d0dXBKMUw5SkR0NG9pWUFNMD0=$9A4hF_YAuvQ5obgVAqNKPCYcEjKensW4IQMovwHtwkF4VYPoHbKxJw!!&t=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBpZCI6IjI5MjU4IiwidGltZXN0YW1wIjoxNTcwODYyNzg0LCJub25jZSI6IlJiUmVLV1JpNU4ifQ.TF0oshJiESmtvyTn-FYG9UHKkdiYaWwi8lM5IukEpnM
+//             var new_link = "http://kdoc.cnki.net/kdoc" + cnt_link.substring(cnt_link.indexOf("kns/", cnt_link) + 3, cnt_link.length);
+//             console.log(new_link);
+// //             xmlhttp.open("GET", new_link, true);
+//             xmlhttp.open("POST", cnt_link, true);
+//             xmlhttp.send();
+//         };
         // 在线阅读的按钮
-        var readonline = downloadpart.childNodes[2];// 这里因为我们已经删掉了“分章下载”，“在线阅读”就变成第2个子结点了
+        var readonline = downloadpart.childNodes[3];
         // 修改外观
-        readonline.classList.add('icon-dlcrsp');
+        readonline.classList.add('icon-phone');
         readonline.classList.add('xml');
         readonline.classList.remove('icon-dlGreen');
-        //
+
         ////////////////////////// dllink是底栏的部分
         var dllink = document.getElementById("wxDlToolbar").childNodes[1].childNodes[1];
         // 整本下载的按钮
@@ -107,13 +129,46 @@ else {
         dlpdf.href = pdf_link;
         dlpdf.classList.add('icon-dlpdf');
         dlpdf.classList.remove('icon-dlBlue');
-        // 分章下载我觉得没什么用所以我就替你们删掉了_(:з」∠)_
-        dllink.removeChild(dllink.childNodes[2]);
+        // 分章下载可以用来获取目录
+        dlChapter = dllink.childNodes[2];
+        dlChapter.innerHTML = dlChapter.innerHTML.replace("分章", "目录");
+        dlChapter.classList.remove('icon-dlBlue');
+        dlChapter.classList.add('icon-dlcrsp');
+        dlChapter.classList.add('xml');
         // 在线阅读的按钮
-        readonline = dllink.childNodes[2];// 同上, “在线阅读”是第2个子结点
+        readonline = dllink.childNodes[3];
         // 修改外观
-        readonline.classList.add('icon-dlcrsp');
+        readonline.classList.add('icon-phone');
         readonline.classList.add('xml');
         readonline.classList.remove('icon-dlGreen');
     }
+}
+
+// 下面是在目录页面加一个下载按钮
+if (myurl.includes("kdoc")) {
+    var title = document.getElementsByClassName("DBlueText")[0];
+    var downCnt = document.createElement("button");
+    downCnt.innerHTML = "下载目录索引";
+    downCnt.id = "downCnt";
+    downCnt.style = "height: 32px; padding: 0 15px; background-color: #1890ff; border-color: #1890ff; color: #fff; font-size: 14px; border-radius: 4px; text-shadow: 0 -1px 0 rgba(0,0,0,0.12); box-shadow: 0 2px 0 rgba(0,0,0,0.045); margin: 10px";
+    downCnt.onclick = function() {
+        var cnt_list = document.getElementById("downCnt").nextSibling.childNodes[1].childNodes;
+        // 要写的内容
+        var contents = "";
+        for (var i = 0; i < cnt_list.length - 1; i++) { // 长度减一, 因为最后一个是text(???这又是什么神仙操作)
+            var cnt_item = cnt_list[i].childNodes[1].childNodes[1];
+            cnt_item = cnt_item.innerHTML;
+            var cnt_page = cnt_list[i].childNodes[3].childNodes[0].textContent.trim().split("-")[0]; // 知网的目录给的是个范围, 正常只需要前半部分
+            contents = contents + cnt_item.trim().replace(/&nbsp;/g, " ") + "\t" + cnt_page + "\r\n";
+        }
+        var data = new Blob([contents],{type:"text/plain; charset=UTF-8"});
+        var downloadUrl = window.URL.createObjectURL(data);
+        var anchor = document.createElement("a");
+        anchor.href = downloadUrl;
+        anchor.download = "目录索引_" + title.innerHTML.trim().replace(/&nbsp;/g, "");
+        anchor.click();
+        window.URL.revokeObjectURL(data);
+        window.alert("目录索引已保存, 请使用FreePic2Pdf软件将目录整合到PDF中");
+    };
+    title.parentElement.insertBefore(downCnt, title.nextElementSibling);
 }
